9eee2c4f550fa48432afa3313afe3ce2001f1c39
Dan Boger <dan@peeron.com>
2014-05-10 14:37:26 -0700

    Clean up namespaces

--- utils.py
+++ utils.py
@@ -3,26 +3,26 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
 
 from apiclient.discovery import build
-from datetime import datetime, timedelta, tzinfo
+import datetime
 import httplib2
 
 class Utils:
     @classmethod
     def saturday(cls):
         """Return this week's saturday."""
-        d = datetime.today().replace(hour=20, minute=0, second=0)
+        d = datetime.datetime.today().replace(hour=20, minute=0, second=0)
         weekday = d.weekday()
         if weekday == 6:
-            return d + timedelta(days=6)
+            return d + datetime.timedelta(days=6)
         elif weekday < 5:
-            return d + timedelta(days=5-weekday)
+            return d + datetime.timedelta(days=5-weekday)
         else:
             return d
 
     @classmethod
     def now(cls):
         """Return current datetime, adjusted for timezone."""
-        now = datetime.now()
+        now = datetime.datetime.now()
         return now + Pacific_tzinfo().utcoffset(now)
 
     @classmethod
@@ -36,28 +36,28 @@ class Utils:
         service = build('calendar', 'v3', http=http)
         return service
 
-class Pacific_tzinfo(tzinfo):
+class Pacific_tzinfo(datetime.tzinfo):
     """Implementation of the Pacific timezone."""
     def utcoffset(self, dt):
-        return timedelta(hours=-8) + self.dst(dt)
+        return datetime.timedelta(hours=-8) + self.dst(dt)
 
     def _FirstSunday(self, dt):
         """First Sunday on or after dt."""
-        return dt + timedelta(days=(6-dt.weekday()))
+        return dt + datetime.timedelta(days=(6-dt.weekday()))
 
     def dst(self, dt):
         # 2 am on the second Sunday in March
-        dst_start = self._FirstSunday(datetime(dt.year, 3, 8, 2))
+        dst_start = self._FirstSunday(datetime.datetime(dt.year, 3, 8, 2))
         # 1 am on the first Sunday in November
-        dst_end = self._FirstSunday(datetime(dt.year, 11, 1, 1))
+        dst_end = self._FirstSunday(datetime.datetime(dt.year, 11, 1, 1))
 
         if dst_start <= dt.replace(tzinfo=None) < dst_end:
-            return timedelta(hours=1)
+            return datetime.timedelta(hours=1)
         else:
-            return timedelta(hours=0)
+            return datetime.timedelta(hours=0)
 
     def tzname(self, dt):
-        if self.dst(dt) == timedelta(hours=0):
+        if self.dst(dt) == datetime.timedelta(hours=0):
             return "PST"
         else:
             return "PDT"
