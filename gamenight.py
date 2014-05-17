0f023dfcb57651ddeabd85dcd38c3fabb54b4f94
Dan Boger <dan@peeron.com>
2014-05-17 12:26:44 -0700

    Dateutil doesn't know 'today', but will default to it anyway.

--- gamenight.py
+++ gamenight.py
@@ -195,7 +195,7 @@ class InvitePage(webapp2.RequestHandler):
         msg = ''
         if args['when']:
             try:
-                args['when'] = parser.parse(args['when'])
+                args['when'] = parser.parse(args['when'].replace('today', ''))
             except ValueError:
                 error = 'Not sure what you mean by "%s"' % args['when']
                 logging.error('Failed to parse when: %s', args['when'])
