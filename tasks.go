fda0e529af90350a2d84421259a8b00b713de8b2
Dan Boger <dan@peeron.com>
2026-02-16 20:34:42 -0800

    Update cron, make sure nag emails don't go out if a gn is actually scheduled

--- tasks.go
+++ tasks.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 // Check if the request is coming from cron, or from an admin. Returns an
@@ -46,6 +47,17 @@ func handleNag(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
         http.Error(w, "Unauthorized", http.StatusForbidden)
 		return
+	}
+
+	if nextGN, err := getNextGamenight(ctx); err != nil {
+		log.Printf("Couldn't find next gamenight: %v", err)
+	} else if nextGN != nil {
+		now := time.Now().In(tz())
+		sun := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, tz()).AddDate(0,0,int(7-now.Weekday()))
+		if nextGN.When().Before(sun) {
+			log.Printf("No need to nag, gn is scheduled for %s", nextGN.When())
+			return
+		}
 	}
 
 	if err := r.ParseForm(); err != nil {
