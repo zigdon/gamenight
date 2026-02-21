ed82c980128ab5426bbf3c907c02b44d326f79a3
Dan Boger <dan@peeron.com>
2026-02-21 14:14:57 -0800

    Fixed the date picker/text field interactions. Can now use either arbitrary text or use the picker.

--- resources/script.js
+++ resources/script.js
@@ -1,6 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    flatpickr("#when", {
-        enableTime: true,
-        dateFormat: "Y-m-d H:i",
-    });
+    const pickerToggleButton = document.getElementById("when-picker-toggle");
+
+    if (pickerToggleButton) {
+        pickerToggleButton.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent default button action (form submission)
+        });
+
+        flatpickr(pickerToggleButton, {
+            enableTime: true,
+            dateFormat: "Y-m-d H:i",
+            defaultHour: 18,
+            onClose: function(selectedDates, dateStr, instance) {
+                if (selectedDates.length > 0) {
+                    document.getElementById("when").value = dateStr;
+                }
+            }
+        });
+    }
 });
